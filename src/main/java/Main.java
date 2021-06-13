66b1f2d932a4462ee6965eeb653f74e827339d89
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-13 18:34:22 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 29 Solution
+ *  Copyright 2021 Brandon Barbour
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean error = true;
+        System.out.print("What is the rate of return? ");
+        int rate = input.nextInt();
+        while (error) {
+            if (input.hasNextInt()) {
+                int years = 72 / rate;
+                System.out.printf("It will take %d years to double your initial investment.", years);
+                error = false;
+            }
+            else {
+                System.out.print("Sorry. that's not a valid input.\nWhat is the rate of return? ");
+                input.next();
+            }
+        }
+    }
+}
